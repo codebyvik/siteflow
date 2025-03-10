206116c6c4f598092495f1668eba77828554b80d
codebyvik <vikasvickycoorg@gmail.com>
2025-03-10 22:22:08 +0530

    added toggle and agency sub pages

--- src/components/global/infobar.tsx
+++ src/components/global/infobar.tsx
@@ -16,6 +16,7 @@ import { Role } from "@prisma/client";
 import { Card } from "../ui/card";
 import { Switch } from "../ui/switch";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { ModeToggle } from "./mode-toggle";
 
 type Props = {
   notifications: NotificationWithUser[] | [];
@@ -109,6 +110,7 @@ const InfoBar = ({ notifications, role, className, subaccountId }: Props) => {
               )}
             </SheetContent>
           </Sheet>
+          <ModeToggle />
         </div>
       </div>
     </>
